c6c1440d3751992e7524dfd1ffdcab1641556d2b
Rikkadruckas <rikka.borges@gmail.com>
2025-11-21 09:50:36 +0000

    Codigo de comeco

---
+++ src/main.cpp
@@ -0,0 +1,214 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+
+#include <VectorXf.h>
+
+#include "MPU6500_Raw.h"
+MPU6500 mpu;
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C // See datasheet for Address
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire1, OLED_RESET, 3400000, 400000);
+
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+static const unsigned char PROGMEM logo_bmp[] =
+{ 0b00000000, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000011, 0b11100000,
+  0b11110011, 0b11100000,
+  0b11111110, 0b11111000,
+  0b01111110, 0b11111111,
+  0b00110011, 0b10011111,
+  0b00011111, 0b11111100,
+  0b00001101, 0b01110000,
+  0b00011011, 0b10100000,
+  0b00111111, 0b11100000,
+  0b00111111, 0b11110000,
+  0b01111100, 0b11110000,
+  0b01110000, 0b01110000,
+  0b00000000, 0b00110000 };
+
+uint32_t interval, last_cycle;
+uint32_t loop_micros;
+uint32_t cycle_count;
+
+void set_interval(float new_interval)
+{
+  interval = new_interval * 1000000L;   // In microseconds
+}  
+
+typedef struct{
+  float angle_x, angle_y, angle_z;
+}datas;
+
+typedef struct {
+  Vec3f w;
+  Vec3f a;
+  uint32_t cycle_time, last_cycle_time; // IMU cycle tracking
+} imu_values_t;
+
+imu_values_t imu;
+datas data_imu;
+
+void getAngle(){
+
+  data_imu.angle_x += imu.w.x*interval;
+  data_imu.angle_y += imu.w.y*interval;
+  data_imu.angle_z += imu.w.z*interval;
+
+}
+
+void setup() 
+{
+  // Builtin LED
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+
+  // Our cycle time
+  set_interval(20e-3); // 20 ms -> 50 Hz
+
+  // IMU initalization
+  const int I2C0_SDA = 20;
+  const int I2C0_SCL = 21;
+  pinMode(I2C0_SDA, INPUT_PULLUP);
+  pinMode(I2C0_SCL, INPUT_PULLUP);
+  
+  Wire.setSDA(I2C0_SDA);
+  Wire.setSCL(I2C0_SCL);
+  Wire.begin();
+  
+  delay(20);
+  
+  MPU6500Setting setting;
+  setting.accel_fs_sel = ACCEL_FS_SEL::A16G;
+  setting.gyro_fs_sel = GYRO_FS_SEL::G2000DPS;
+  setting.fifo_sample_rate = FIFO_SAMPLE_RATE::SMPL_200HZ;
+  setting.gyro_fchoice = 0x03;
+  setting.gyro_dlpf_cfg = GYRO_DLPF_CFG::DLPF_41HZ;
+  setting.accel_fchoice = 0x01;
+  setting.accel_dlpf_cfg = ACCEL_DLPF_CFG::DLPF_45HZ;
+  
+  while (!mpu.setup(0x68, setting)) { 
+    Serial.println("MPU connection failed.");
+    delay(500); // Wait to try again     
+  }
+
+  // OLED initalization
+  const int I2C1_SDA = 18;
+  const int I2C1_SCL = 19;
+  pinMode(I2C1_SDA, INPUT_PULLUP);
+  pinMode(I2C1_SCL, INPUT_PULLUP);
+  
+  Wire1.setSDA(I2C1_SDA);
+  Wire1.setSCL(I2C1_SCL);
+  Wire1.begin();
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  while(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println("SSD1306 allocation failed");
+    delay(500);  // Wait to try again
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  data_imu.angle_x = 0;
+  data_imu.angle_y = 0;
+  data_imu.angle_z = 0;
+}
+
+void loop() 
+{
+  uint8_t b;
+  if (Serial.available()) {  // Only do this if there is serial data to be read
+  
+    b = Serial.read();    
+    //Serial.write(b);
+  } 
+
+  // Do this only every "interval" microseconds 
+  uint32_t now = micros();
+  uint32_t delta = now - last_cycle; 
+  if (delta >= interval) {
+    loop_micros = micros();
+    last_cycle = now;
+    cycle_count++;
+
+    // Read, if ready, the MPU
+    if (mpu.update()) {
+      imu.last_cycle_time = imu.cycle_time;
+      imu.cycle_time = micros();
+
+      imu.w.x = mpu.getGyroX();
+      imu.w.y = mpu.getGyroY();
+      imu.w.z = mpu.getGyroZ();
+
+      imu.a.x = mpu.getAccX();
+      imu.a.y = mpu.getAccY();
+      imu.a.z = mpu.getAccZ();
+    }
+    
+    getAngle();
+
+    // OLED output
+    display.clearDisplay();
+
+    display.setTextSize(1);      // Normal 1:1 pixel scale
+    display.setTextColor(SSD1306_WHITE); // Draw white text
+    display.setCursor(0, 0);     // Start at top-left corner
+    
+    display.printf("Wx %.2f\n", imu.w.x);
+    display.printf("Wy %.2f\n", imu.w.y);
+    display.printf("Wz %.2f\n", imu.w.z);
+
+    display.setCursor(64, 0);     // Start at top-left corner
+    display.printf("Ax %.2f", imu.a.x);
+    display.setCursor(64, 8);
+    display.printf("Ay %.2f", imu.a.y);
+    display.setCursor(64, 16);
+    display.printf("Az %.2f", imu.a.z);
+
+    display.display();
+
+    // Serial output
+    Serial.printf("IMU_dt %d; ", imu.cycle_time - imu.last_cycle_time);
+
+    Serial.printf("Wx %.2f; ", imu.w.x);
+    Serial.printf("Wy %.2f; ", imu.w.y);
+    Serial.printf("Wz %.2f; ", imu.w.z);
+
+    Serial.printf("Ax %.2f; ", imu.a.x);
+    Serial.printf("Ay %.2f; ", imu.a.y);
+    Serial.printf("Az %.2f; ", imu.a.z);
+
+    Serial.printf("Angulo_x : %.2f", data_imu.angle_x);
+    Serial.printf("Angulo_y : %.2f", data_imu.angle_y);
+    Serial.printf("Angulo_z : %.2f", data_imu.angle_z);
+
+    //Serial.print("T ");
+    //Serial.print(mpu.getTemperature(), 2);
+
+    Serial.print("loop ");
+    Serial.print(micros() - now);
+
+    Serial.println();
+  }
+
+}
+
